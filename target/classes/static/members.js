57fcaf7263aeb858678b3d18bc1480df47325f4d
Roberto Tencic <roberto.tencic@outlook.com>
2023-11-01 08:12:18 +0100

    Update

--- target/classes/static/members.js
+++ target/classes/static/members.js
@@ -7,25 +7,18 @@ if (localStorage.getItem(adminUsername) !== null) {
     // Vi loopar igenom listan och lägger till en knapp för varje medlem.
     for (let i = 0; i < li.length; i++) {
         // Alla medlemmar får en ta bort-knapp förutom admin. Vi vill ju inte att man ska kunna ta bort admin som medlem.
-        if (li.item(i).getAttribute("id") !== "admin") {
-            // Skapar en ny knapp.
+        if (li.item(i).getAttribute("id") !== adminUsername) {
+            // Ställer in knappen (länken).
             let button = li.item(i).getElementsByTagName("a").item(0);
-            // Ställer in knappen.
             button.style.display = "inline";
+            button.style.float = "right";
             button.style.backgroundImage = "url('remove.png')";
             button.style.padding = "8px";
             button.style.borderRadius = "9px";
             button.style.border = "1px solid red";
-            button.style.float = "right";
-            button.addEventListener("mouseover", () => {
-                button.style.backgroundColor = "red";
-            });
-            button.addEventListener("mouseout", () => {
-                button.style.backgroundColor = "transparent";
-            });
+            button.addEventListener("mouseover", (e) => (e.target.style.backgroundColor = "red"));
+            button.addEventListener("mouseout", (e) => (e.target.style.backgroundColor = "transparent"));
             button.setAttribute("hidden", false);
-            // Lägger till sist till knappen i DOM:en.
-            li.item(i).appendChild(button);
         }
     }
 }
